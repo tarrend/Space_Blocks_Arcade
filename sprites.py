0dac2392b2f75f1e1487058013dd266eb06d16be
tarrend <275414200+tarrend@users.noreply.github.com>
2026-09-08 15:53:26 +1000

    Ship attributes
    Ship has been given attributes for its image and rect

--- sprites.py
+++ sprites.py
@@ -1,8 +1,17 @@
 import pygame
+from constants import *
 
 # this file contains the different sprites for the game
 class Ship(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, size, rect_size):
         super().__init__()
 
-        
+        # intial variables
+        self.image = pygame.Surface(size)
+        self.image = pygame.transform.rotate(self.image, 45)
+        self.image.fill("#3b96eb")
+
+        self.rect = pygame.Rect((0, 0), rect_size)
+        self.rect.center = pygame.Vector2(WIDTH/2, HEIGHT-100)
+
+
