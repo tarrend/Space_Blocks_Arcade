221baf62efe0242813fb8e34e383ab4457f1cc79
tarrend <275414200+tarrend@users.noreply.github.com>
2026-09-08 15:57:44 +1000

    Player group
    for now the player appears near the bottom of the screen

--- gameplay.py
+++ gameplay.py
@@ -1,11 +1,19 @@
 import pygame
 from constants import *
+import sprites
 
 
 # this script is dedicated for running gameplay elements
 # however it does not contain sprites or many classes it is mainly just running
 # that being said it does contain some basic objects
 
+# groups
+player_group = pygame.sprite.GroupSingle()
+
+# add sprites to groups
+player_group.add(sprites.Ship())
+
+# title text
 title_text_font = pygame.Font(None, 50)
 title_text_surface = title_text_font.render("Space Blocks Arcade!", True, "white")
 title_text_rect = title_text_surface.get_rect(center = pygame.Vector2(WIDTH/2, 100))
@@ -14,6 +22,9 @@ def rendering(screen):
 
     # displaying different text
     screen.blit(title_text_surface, title_text_rect)
+
+    # rendering the different groups
+    player_group.draw(screen)
     
 
 def updating():
--- sprites.py
+++ sprites.py
@@ -3,15 +3,15 @@ from constants import *
 
 # this file contains the different sprites for the game
 class Ship(pygame.sprite.Sprite):
-    def __init__(self, size, rect_size):
+    def __init__(self):
         super().__init__()
 
         # intial variables
-        self.image = pygame.Surface(size)
+        self.image = pygame.Surface((50, 50))
         self.image = pygame.transform.rotate(self.image, 45)
         self.image.fill("#3b96eb")
 
-        self.rect = pygame.Rect((0, 0), rect_size)
-        self.rect.center = pygame.Vector2(WIDTH/2, HEIGHT-100)
+        self.rect = pygame.Rect((0, 0), (40, 40))
+        self.rect.center = pygame.Vector2(WIDTH/2, HEIGHT-200)
 
 
